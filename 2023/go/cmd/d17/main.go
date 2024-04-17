0240faa4f0f8df2217f7cb94e6809ec15cd401c3
Portablejim <portablejim@jamesmckay.id.au>
2024-04-17 22:59:32 +1000

    2023 Day 17: Cleanup code

--- 2023/go/cmd/d17/main.go
+++ 2023/go/cmd/d17/main.go
@@ -15,8 +15,6 @@ type GraphNode struct {
 	x                  int
 	cost               int
 	visited            bool
-	visited_v          bool
-	visited_h          bool
 	isPending          bool
 	lowest_cost        int
 	lowest_cost_path   string
@@ -26,25 +24,6 @@ type GraphNode struct {
 	lowest_cost_path_v string
 }
 
-type Coord struct {
-	y int
-	x int
-}
-
-func (c Coord) move(direction string) Coord {
-	switch direction {
-	case "N":
-		c.y -= 1
-	case "S":
-		c.y += 1
-	case "E":
-		c.x -= 1
-	case "W":
-		c.x += 1
-	}
-	return c
-}
-
 type CruciblePosition struct {
 	direction   string
 	history     string
@@ -52,6 +31,16 @@ type CruciblePosition struct {
 	y           int
 	x           int
 	is_starting bool
+}
+
+func generatePositionKey(current_position CruciblePosition) string {
+    var current_position_key string
+    if current_position.direction == "N" || current_position.direction == "S" {
+        current_position_key = fmt.Sprintf("V,%d,%d", current_position.y, current_position.x)
+    } else {
+        current_position_key = fmt.Sprintf("H,%d,%d", current_position.y, current_position.x)
+    }
+    return current_position_key
 }
 
 func moveDirection(direction string, pos CruciblePosition) CruciblePosition {
@@ -67,140 +56,6 @@ func moveDirection(direction string, pos CruciblePosition) CruciblePosition {
 	} else {
 		return CruciblePosition{"0", next_history, pos.cost, pos.y, pos.x, false}
 	}
-}
-
-func getNextDirections(direction rune, previous_directions string, min_before_turn int, max_before_turn int) []rune {
-	output := []rune{}
-
-	// Not enough directions exist yet before turning.
-	if len(previous_directions) < min_before_turn {
-		//fmt.Printf("Forced straight 1: %d < %d\n", len(previous_directions), min_before_turn)
-		output = append(output, direction)
-		return output
-	}
-
-	// Count num taken in each direction.
-	count_same_direction := 0
-	for i := len(previous_directions) - 1; i >= 0 && count_same_direction <= (max_before_turn+1); i -= 1 {
-		if previous_directions[i] != byte(direction) {
-			break
-		}
-		count_same_direction += 1
-	}
-
-	// Not enough of the same direction to turn.
-	if count_same_direction < min_before_turn {
-		output = append(output, direction)
-		//fmt.Printf("Forced straight 2: %d < %d\n", count_same_direction, min_before_turn)
-		return output
-	}
-
-	can_move_same := true
-	if count_same_direction >= max_before_turn {
-		//fmt.Printf("Forced turn: %d > %d\n", count_same_direction, max_before_turn)
-		can_move_same = false
-	}
-
-	if can_move_same {
-		output = append(output, direction)
-	}
-	if direction == 'N' || direction == 'S' {
-		output = append(output, 'E', 'W')
-	} else if direction == 'E' || direction == 'W' {
-		output = append(output, 'N', 'S')
-	}
-
-	//fmt.Printf("Next dirs: %v\n", output)
-
-	return output
-}
-
-func getLowestCost(s_i int, starting_nodes []CruciblePosition, ending_nodes_list []CruciblePosition, graph_nodes [][]GraphNode, min_before_turn int, max_before_turn int) (int, string) {
-	ending_node := ending_nodes_list[s_i]
-
-	pending_positions := []CruciblePosition{}
-	pending_positions = append(pending_positions, starting_nodes...)
-
-	// Handle the positions
-	for len(pending_positions) > 0 {
-		prev_position := pending_positions[0]
-		prev_node := graph_nodes[prev_position.y][prev_position.x]
-		pending_positions = pending_positions[1:]
-
-		min_moves := 1
-		if len(prev_position.history) == 0 || prev_position.direction != string(prev_position.history[len(prev_position.history)-1]) {
-			min_moves = min_before_turn
-		}
-		out_of_range := false
-		current_pos := prev_position
-		// Skip over any positions where we can't turn.
-		for i := 0; i < min_moves; i += 1 {
-			prev_position = current_pos
-			current_pos = moveDirection(prev_position.direction, prev_position)
-			//fmt.Printf("Current pos: %v\n", current_pos)
-			if current_pos.y >= len(graph_nodes) || current_pos.x >= len(graph_nodes[0]) || current_pos.y < 0 || current_pos.x < 0 || current_pos.direction == "0" {
-				// Out of range, it expires.
-				//fmt.Printf("expired r: %v\n", current_end)
-				//fmt.Printf("Expire (invalid): %v\n", current_pos)
-				out_of_range = true
-				break
-			}
-			//fmt.Printf("Add cost %d + %d at %d,%d (%s)\n", current_pos.cost, graph_nodes[current_pos.y][current_pos.x].cost, current_pos.y, current_pos.x, current_pos.history)
-			current_pos.cost += graph_nodes[current_pos.y][current_pos.x].cost
-		}
-		if out_of_range {
-			continue
-		}
-		new_cost := current_pos.cost
-
-		current_node := &graph_nodes[current_pos.y][current_pos.x]
-		//fmt.Printf("N: %v %v %v %d\n", prev_node, current_pos, *current_node, new_cost)
-		if new_cost < 0 || new_cost >= current_node.lowest_cost {
-			// Higher cost, don't continue.
-			//fmt.Printf("Expire (cost): %d > %d | %s | %s\n", new_cost, current_node.lowest_cost, current_pos.history, current_node.lowest_cost_path)
-			continue
-		}
-
-		current_node.visited = true
-		current_node.lowest_cost = new_cost
-		current_node.lowest_cost_path = current_pos.history
-
-		if current_pos.y == ending_node.y && current_pos.x == ending_node.x {
-			// Found the end.
-			//return current_node.lowest_cost, current_node.lowest_cost_path
-			fmt.Printf("Possible lowest: %v %v %v\n", current_pos, *current_node, prev_node)
-			continue
-		}
-
-		for _, next_dir := range getNextDirections(rune(current_pos.direction[0]), current_pos.history, min_before_turn, max_before_turn) {
-			pending_positions = append(pending_positions, CruciblePosition{string(next_dir), current_pos.history, current_pos.cost, current_pos.y, current_pos.x, false})
-		}
-		//fmt.Printf("Next: %v\n", pending_positions)
-		//fmt.Printf("Next: %d\n", len(pending_positions))
-
-		/*
-		   fmt.Printf("next: %v\n", laser_ends)
-		   for _,floor_tile_line := range floor_tiles {
-		       for _,floor_tile := range floor_tile_line {
-		           if floor_tile.visited_count > 0 {
-		               if floor_tile.visited_count > 1 {
-		                   //fmt.Printf("█")
-		                   fmt.Printf("#")
-		               } else {
-		                   fmt.Printf("#")
-		               }
-		           } else {
-		               fmt.Printf(" ")
-		           }
-		       }
-		       fmt.Printf("\n")
-		   }
-		*/
-	}
-
-	return graph_nodes[ending_node.y][ending_node.x].lowest_cost, graph_nodes[ending_node.y][ending_node.x].lowest_cost_path
-
-	//return math.MaxInt64, "ERROR"
 }
 
 func main() {
@@ -225,7 +80,7 @@ func main() {
 		graph_node_row := []GraphNode{}
 		for c_num := 0; c_num < len(f_line); c_num += 1 {
 			node_weight := f_line[c_num] - '0'
-			graph_node_row = append(graph_node_row, GraphNode{l_num, c_num, int(node_weight), false, false, false, false, math.MaxInt64, "", math.MaxInt64, "", math.MaxInt64, ""})
+			graph_node_row = append(graph_node_row, GraphNode{l_num, c_num, int(node_weight), false, false, math.MaxInt64, "", math.MaxInt64, "", math.MaxInt64, ""})
 		}
 		graph_nodes = append(graph_nodes, graph_node_row)
 	}
@@ -234,23 +89,16 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error when reading file.\n")
 		return
 	}
-	graph_nodes[0][0].lowest_cost = 0
 	graph_nodes[0][0].lowest_cost_h = 0
 	graph_nodes[0][0].lowest_cost_v = 0
-	//fmt.Printf("floor_tile_rows: %v\n", floor_tiles)
 
 	max_y := len(graph_nodes) - 1
 	max_x := len(graph_nodes[0]) - 1
 
-	//starting_nodes_list := [][]CruciblePosition{{{'E', "", 0, 0, 0, true}, {'S', "", 0, 0, 0, true}}}
-	//ending_nodes_list := []CruciblePosition{{'0', "", 0, max_y, max_x, false}}
-
-	//starting_nodes := []CruciblePosition{{"E", "", 0, 0, 0, true}, {"S", "", 0, 0, 0, true}}
 	starting_node := CruciblePosition{"0", "", 0, 0, 0, true}
 	ending_node := CruciblePosition{"0", "", 0, max_y, max_x, false}
 
 	total := math.MaxInt64
-	first_total := math.MaxInt64
 
 	min_before_turn := 1
 	max_before_turn := 3
@@ -275,13 +123,7 @@ func main() {
 			pending_positions = pending_positions[1:]
 
 			//current_position_key := fmt.Sprintf("%v", current_position)
-			current_position_key := fmt.Sprintf("%s,%d,%d", current_position.direction, current_position.y, current_position.x)
-			if current_position.direction == "N" || current_position.direction == "S" {
-				current_position_key = fmt.Sprintf("V,%d,%d", current_position.y, current_position.x)
-			} else {
-				current_position_key = fmt.Sprintf("H,%d,%d", current_position.y, current_position.x)
-			}
-			//fmt.Printf("Pos: %s | p %d\n", current_position_key, len(pending_positions))
+			current_position_key := generatePositionKey(current_position)
 
 			graph_nodes[current_position.y][current_position.x].isPending = false
 			pending_pos_map[current_position_key] = false
@@ -299,7 +141,6 @@ func main() {
 				next_position.direction = dir
 				for i := 0; i < max_before_turn; i += 1 {
 					next_position = moveDirection(dir, next_position)
-					//fmt.Printf("Next: %v | p %d\n", next_position, len(pending_positions))
 
 					isPosRangeBad := next_position.y >= len(graph_nodes)
 					isPosRangeBad = isPosRangeBad || next_position.x >= len(graph_nodes[0])
@@ -308,33 +149,21 @@ func main() {
 					isPosRangeBad = isPosRangeBad || next_position.direction == "0"
 					if isPosRangeBad {
 						// Out of range, it expires.
-						//fmt.Printf("[%d] Expire (invalid): %v\n", i, next_position)
 						break
 					}
 
 					next_node := &graph_nodes[next_position.y][next_position.x]
-					//fmt.Printf("[%d] Cost: %d + %d = %d\n", i, next_cost, next_node.cost, next_cost + next_node.cost)
 					next_cost += next_node.cost
 					// If minimum distance before turning has been reached
 					if (i + 1) >= min_before_turn {
-						//fmt.Printf("Reached min\n")
 						next_position.cost = next_cost
 
 						if next_node.visited {
-							//fmt.Printf("[%d] Expire (visited): %v\n", i, next_position)
 							continue
 						}
 
-						//next_node_key := fmt.Sprintf("%v", *&next_position)
-						//next_node_key := fmt.Sprintf("%s,%d,%d,%d", next_position.direction, next_position.y, next_position.x, next_position.cost)
-						var next_node_key string
-						if next_position.direction == "N" || next_position.direction == "S" {
-							next_node_key = fmt.Sprintf("V,%d,%d", next_position.y, next_position.x)
-						} else {
-							next_node_key = fmt.Sprintf("H,%d,%d", next_position.y, next_position.x)
-						}
-						//next_node_pending, next_node_pending_found := pending_pos_map[next_node_key]
-						next_node_pending, next_node_pending_found := pending_pos_map[next_node_key]
+                        next_position_key := generatePositionKey(next_position)
+						next_node_pending, next_node_pending_found := pending_pos_map[next_position_key]
 						if !next_node_pending_found {
 							next_node_pending = false
 						}
@@ -357,13 +186,8 @@ func main() {
                                 // If pending, replace the position.
                                 for pos_i := 0; pos_i < len(pending_positions); pos_i += 1 {
                                     candidate_pos := pending_positions[pos_i]
-                                    var candidate_pos_key string
-                                    if candidate_pos.direction == "N" || candidate_pos.direction == "S" {
-                                        candidate_pos_key = fmt.Sprintf("V,%d,%d", candidate_pos.y, candidate_pos.x)
-                                    } else {
-                                        candidate_pos_key = fmt.Sprintf("H,%d,%d", candidate_pos.y, candidate_pos.x)
-                                    }
-                                    if next_node_key == candidate_pos_key {
+                                    candidate_pos_key := generatePositionKey(candidate_pos)
+                                    if next_position_key == candidate_pos_key {
                                         pending_positions[pos_i] = next_position
                                     }
                                 }
@@ -378,25 +202,17 @@ func main() {
 								}
 								fmt.Printf("[%d] Candidate: %d | %s\n", i, next_cost, next_position.history)
 							}
-						} else {
-							//fmt.Printf("[%d] Not cheaper: %d < %d %s %v\n", i, next_cost, next_node.lowest_cost, next_position.history, next_node)
 						}
 
 						if next_position.direction == "N" || next_position.direction == "S" {
 							if next_node.lowest_cost_v < math.MaxInt64 && !next_node_pending {
-								//fmt.Printf("[%d] Appending next: %v\n", i, next_position)
 								pending_positions = append(pending_positions, next_position)
-								pending_pos_map[next_node_key] = true
-							} else {
-								//fmt.Printf("Not cheaper: %s %t %v\n", next_node_key, next_node_pending, *next_node)
+								pending_pos_map[next_position_key] = true
 							}
 						} else {
 							if next_node.lowest_cost_h < math.MaxInt64 && !next_node_pending {
-								//fmt.Printf("[%d] Appending next: %v\n", i, next_position)
 								pending_positions = append(pending_positions, next_position)
-								pending_pos_map[next_node_key] = true
-							} else {
-								//fmt.Printf("Not cheaper: %s %t %v\n", next_node_key, next_node_pending, *next_node)
+								pending_pos_map[next_position_key] = true
 							}
 						}
 					}
@@ -426,26 +242,14 @@ func main() {
 					return 1
 				}
 			})
-			//if len(pending_positions) < 10 {
-			//    fmt.Fprintf(os.Stderr, "Pending: %v\n", pending_positions)
-			//} else {
-			//    fmt.Fprintf(os.Stderr, "Pending: %d %v\n", len(pending_positions), pending_positions[:2])
-			//}
-			if loop_num%10_000 == 0 {
-				if len(pending_positions) < 10 {
-					fmt.Printf("Pending: %v\n", pending_positions)
-				} else {
-					fmt.Printf("Pending: %d %v\n", len(pending_positions), pending_positions[:2])
-				}
-			}
 		}
 
 		for i_y, graph_line := range graph_nodes {
 			for i_x, graph_nde := range graph_line {
 				if graph_nde.lowest_cost < 10_000 {
-					fmt.Printf("%d:%4d | ", graph_nde.cost, graph_nde.lowest_cost)
+					fmt.Printf("%d:%3d|", graph_nde.cost, graph_nde.lowest_cost)
 				} else {
-					fmt.Printf("%d:     | ", graph_nde.cost)
+					fmt.Printf("%d:   |", graph_nde.cost)
 				}
 				graph_nodes[i_y][i_x].visited = false
 			}
@@ -454,9 +258,8 @@ func main() {
 
 		total = graph_nodes[ending_node.y][ending_node.x].lowest_cost
 		*visualise = graph_nodes[ending_node.y][ending_node.x].lowest_cost_path
-		first_total = total
 
-		fmt.Printf("T: p1 %d p2 %d \n", first_total, total)
+		fmt.Printf("T: %d\n", total)
 	}
 
 	if len(*visualise) > 0 {

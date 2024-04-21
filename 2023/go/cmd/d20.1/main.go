a913a525dc9eae1eccc1655ef72fec0926b51417
Portablejim <portablejim@jamesmckay.id.au>
2024-04-21 22:37:28 +1000

    Finish 2023 Day 20 Part 1

--- 2023/go/cmd/d20.1/main.go
+++ 2023/go/cmd/d20.1/main.go
@@ -21,10 +21,11 @@ type Module struct {
 type Pulse struct {
     isHigh bool
     target string
+    source string
 }
 
 type StateData struct {
-    prevState string
+    nextState string
     stepNumber int
     countHigh int
     countLow int
@@ -53,14 +54,16 @@ func handleButtonPush(moduleMap map[string]Module) (map[string]Module, int, int)
     countHigh := 0
     countLow := 0
 
-    pulseQueue := []Pulse{{false, "broadcaster"}}
+    pulseQueue := []Pulse{{false, "broadcaster", "button"}}
 
     for len(pulseQueue) > 0 {
         currentPulse := pulseQueue[0]
         pulseQueue = pulseQueue[1:]
 
+        //pulseLevelStr := "L"
         if currentPulse.isHigh {
             countHigh += 1
+            //pulseLevelStr = "H"
         } else {
             countLow += 1
         }
@@ -68,12 +71,52 @@ func handleButtonPush(moduleMap map[string]Module) (map[string]Module, int, int)
 
         currentModule, currentModuleExists := moduleMap[currentPulse.target]
         if !currentModuleExists {
+            //fmt.Printf("P I: (from %s) %s -%s-> ?!?!\n", currentPulse.source, currentPulse.target, pulseLevelStr)
             continue
         }
+        //fmt.Printf("P I: %s -%s-> %s %v\n", currentPulse.source, pulseLevelStr, currentPulse.target, currentModule.targetModules)
 
-        if currentModule.typeName == "broadcaster" {
+        if currentModule.moduleName == "button" || currentModule.moduleName == "broadcaster" {
             for _,nextModuleName := range currentModule.targetModules {
-                pulseQueue = append(pulseQueue, Pulse{currentPulse.isHigh, nextModuleName})
+                //fmt.Printf("P O: %s -%s-> %s\n", currentPulse.target, pulseLevelStr, nextModuleName)
+                pulseQueue = append(pulseQueue, Pulse{currentPulse.isHigh, nextModuleName, currentModule.moduleName})
+            }
+        } else if currentModule.typeName == "%" {
+            // Flip-flop
+            if !currentPulse.isHigh {
+                currentModule.outputState = !currentModule.outputState
+                moduleMap[currentPulse.target] = currentModule
+                for _,nextModuleName := range currentModule.targetModules {
+                    /*
+                    outputPulseLevelStr := "L"
+                    if currentModule.outputState {
+                        outputPulseLevelStr = "H"
+                    }
+                    fmt.Printf("P O: %s -%s-> %s\n", currentPulse.target, outputPulseLevelStr, nextModuleName)
+                    */
+                    pulseQueue = append(pulseQueue, Pulse{currentModule.outputState, nextModuleName, currentModule.moduleName})
+                }
+            } else {
+                //fmt.Printf("P O: %s END (%d)\n", currentPulse.target, len(pulseQueue))
+            }
+        } else if currentModule.typeName == "&" {
+            // Conjunction
+            currentModule.inputStates[currentPulse.source] = currentPulse.isHigh
+
+            areAllInputsHigh := true
+            for _,inputIsHigh := range currentModule.inputStates {
+                areAllInputsHigh = areAllInputsHigh && inputIsHigh
+            }
+
+            for _,nextModuleName := range currentModule.targetModules {
+                /*
+                outputPulseLevelStr := "L"
+                if !areAllInputsHigh {
+                    outputPulseLevelStr = "H"
+                }
+                fmt.Printf("P O: %s -%s-> %s\n", currentPulse.target, outputPulseLevelStr, nextModuleName)
+                */
+                pulseQueue = append(pulseQueue, Pulse{!areAllInputsHigh, nextModuleName, currentModule.moduleName})
             }
         }
     }
@@ -124,11 +167,71 @@ func main() {
         }
     }
 
+    stateMap := map[string]StateData{}
 
-    //stateMap := map[string]StateData{}
+    totalHigh := 0
+    totalLow := 0
 
+    currentState := fmt.Sprintf("%v", moduleMap)
+    stateMap[currentState] = StateData{"", 0, 0, 0, 0, 0}
+
+    loopSize := -1
+
+    pressCount := 1_000
+    for i := 0; i < pressCount; i += 1 {
+        //fmt.Printf("I: %d %v\n", i, currentState)
+        currentStateData, currentStateInMap := stateMap[currentState]
+        if currentStateInMap && currentStateData.nextState != "" {
+            if loopSize < 0 {
+                loopSize = i - currentStateData.stepNumber
+                fmt.Printf("Loop: %d %d\n", i, currentStateData.stepNumber)
+
+                numRemaining := pressCount - i
+                loopCount := (numRemaining / loopSize)
+                numRemainAfter := numRemaining % loopSize
+                loopTotalHigh := totalHigh - currentStateData.totalHigh - currentStateData.countHigh
+                loopTotalLow := totalLow - currentStateData.totalLow
+
+                totalHigh += loopCount * loopTotalHigh
+                totalLow += loopCount * loopTotalLow
+                i = pressCount - numRemainAfter
+                //fmt.Printf("Loop found %d %d %d\n", loopSize, numRemaining, numRemainAfter)
+            } else {
+                //fmt.Printf("Loop used\n")
+            }
+            currentState = currentStateData.nextState
+
+
+        } else {
+            _, pushHighCount, pushLowCount := handleButtonPush(moduleMap)
+
+            totalHigh += pushHighCount
+            totalLow += pushLowCount
+
+            //fmt.Printf("State: %s\n", currentState)
+            nextState := fmt.Sprintf("%v", moduleMap)
+
+
+            if currentStateData.nextState == "" {
+                currentStateData.nextState = nextState
+                stateMap[currentState] = currentStateData;
+            }
+
+            nextStateData, nextStateInMap := stateMap[nextState]
+            if !nextStateInMap {
+                fmt.Printf("Current state not in map\n")
+                nextStateData = StateData{"", i+1, pushHighCount, pushLowCount, totalHigh, totalLow}
+                stateMap[nextState] = nextStateData
+            }
+            currentState = nextState
+
+            //fmt.Printf("State: %s\n", currentState)
+        }
+    }
+
+    total = totalLow * totalHigh
 
     //fmt.Printf("node_list: %v\n", node_list)
-    fmt.Printf("T: %d\n", total)
+    fmt.Printf("T: %d %d %d\n", totalHigh, totalLow, total)
 }
 
